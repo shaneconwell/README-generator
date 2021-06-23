360fea06f3b26f04711be87908a85ee9bb1abd80
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-06-23 19:02:41 -0400

    Added all question logic, read me filler is set up, and license badges. Now i need to figure out how to connect everything

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project:',
+        name: 'description',
+    },
+
+    // // this one is the multiple choise license prompt
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        choices:['MIT'
+            ,'APACHE 2.0'
+            ,'GPL 3.0'
+            ,'BSD 3'
+            ,'None'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contribute',
+    }
+]
+
+inquirer.prompt(questions)
+
+.then((answers) => {
+    
+    console.log(answers);
+    
+  })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "mit":
+      console.log("https://img.shields.io/badge/license-MIT-brightgreen");
+      break;
+    case "apache":
+      console.log(
+        "https://img.shields.io/badge/license-APACHE%202.0-brightgreen"
+      );
+      break;
+    case "gpl":
+      console.log("https://img.shields.io/badge/license-GPL%203.0-brightgreen");
+      break;
+    case "bsd":
+      console.log("https://img.shields.io/badge/license-BSD%203-brightgreen");
+      break;
+    default:
+      console.log("none");
+  }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'mit':
+      console.log("https://opensource.org/licenses/MIT");
+      break;
+    case 'apache':
+      console.log("https://opensource.org/licenses/Apache-2.0");
+      break;
+    case 'gpl':
+      console.log("https://opensource.org/licenses/GPL-3.0");
+      break;
+    case 'bsd':
+      console.log("https://opensource.org/licenses/BSD-3-Clause");
+      break;
+    default:
+      console.log('none');
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,9 +52,57 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# Title 
 
-`;
-}
+  ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+  To install necessary dependencies, run the following command:
+
+  // come back to this
+
+  \```md\  
+  ${data.installation}
+  \```\
+
+  ## Usage
+  ${data.usage}
+
+  ##License
+  This project is licesed under the ${data.license} license.
+
+  ##Contributing
+  ${data.contribute}
+
+  ##Tests
+  To run tests, run the following command:
+
+  // come back to this
+  md  
+  ${data.test}
+
+  ##Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.username}.
+
+  `;
+  }
+
+
+
+  renderLicenseBadge(process.argv[2]);
+  renderLicenseLink(process.argv[2]);
+// fs.writeFile('log.txt', process.argv[2], (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
+
 
 module.exports = generateMarkdown;
